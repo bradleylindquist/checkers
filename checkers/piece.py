33c1e780237e454d512046a4cc20fbd0e7c43697
Student <Student@Admins-MacBook-Air.local>
2024-01-02 15:02:43 -0500

    move pieces

--- checkers/piece.py
+++ checkers/piece.py
@@ -34,6 +34,11 @@ class Piece:
       pygame.draw.circle(win, self.color, (self.x, self.y), radius)
       if self.king:
         win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height//2))
+        
+      def move(self, row, col):
+        self.row = row
+        self.col = col
+        self.calc_pos()
       
     def __repr__(self):
       return str(self.color)
